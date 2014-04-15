583cb873cf911b44fe25cf1ed3cb0647b8844ce7
Long <121382647@163.com>
2014-04-15 21:45:00 +0800

    Added files.

---
+++ AMMethod2Implement/AMMethod2Implement.h
@@ -0,0 +1,13 @@
+//
+//  AMMethod2Implement.h
+//  AMMethod2Implement
+//
+//  Created by Mellong on 14-4-15.
+//  Copyright (c) 2014年 Tendencystudio. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface AMMethod2Implement : NSObject
+
+@end
---
+++ AMMethod2Implement/AMMethod2Implement.m
@@ -0,0 +1,64 @@
+//
+//  AMAMMethod2Implement.m
+//  AMAMMethod2Implement
+//
+//  Created by Mellong on 14-4-15.
+//    Copyright (c) 2014年 Tendencystudio. All rights reserved.
+//
+
+#import "AMMethod2Implement.h"
+
+static AMMethod2Implement *sharedPlugin;
+
+@interface AMMethod2Implement()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation AMMethod2Implement
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
