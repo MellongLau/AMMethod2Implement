f230ec4f5a96a29ac615988bd875465d0f4e354c
MellongLau <121382647@qq.com>
2015-01-21 11:50:35 +0800

    Added const string implement support.

--- AMMethod2Implement/AMMethod2Implement.m
+++ AMMethod2Implement/AMMethod2Implement.m
@@ -55,33 +55,89 @@ static AMMethod2Implement *sharedPlugin;
 // For menu item:
 - (void)doImplementMethodAction
 {
-    NSString *methodName = [AMIDEHelper getCurrentSelectMethod];
-    if ([methodName matches:@"^[-+].+"]) {
-        methodName = [methodName stringByReplacingOccurrencesOfString:@";" withString:@""];
-        NSLog(@"%@", methodName);
-        [AMIDEHelper openFile:[AMIDEHelper getMFilePathOfCurrentEditFile]];
-        
-        NSTextView *textView = [AMXcodeHelper currentSourceCodeTextView];
-        NSRange textRange = [textView.textStorage.string rangeOfString:methodName options:NSCaseInsensitiveSearch];
-        if (textRange.location == NSNotFound) {
-            NSString *className = [AMIDEHelper getCurrentClassName];
-            NSString *implementationString = [NSString stringWithFormat:@"@implementation %@", className];
-            NSString *endString = @"@end";
-            NSRange startRange = [textView.textStorage.string rangeOfString:implementationString options:NSCaseInsensitiveSearch];
-            NSRange searchRange = NSMakeRange(startRange.location, textView.textStorage.string.length - startRange.location);
-            NSRange endRange = [textView.textStorage.string rangeOfString:endString options:NSCaseInsensitiveSearch range:searchRange];
-            
-            [textView scrollRangeToVisible:endRange];
-            
-            NSString *newString = [NSString stringWithFormat:@"\n%@{\n    \n}\n\n%@", methodName, endString];
-            [textView insertText:newString replacementRange:endRange];
-            
-            [AMIDEHelper selectText:methodName];
-        }
-        [AMIDEHelper selectText:methodName];
+    NSString *selectString = [AMIDEHelper getCurrentSelectMethod];
+    NSDictionary *implementMap = @{@"^[-+].+":@"implementObjcMethodWithCurrentSelectString:",
+                                   @"^extern\\s*?NSString\\s*?\\*\\s*?const\\s*?.+":@"implementConstStringWithCurrentSelectString:"
+                                   };
 
-    }
+    [implementMap enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        if ([selectString matches:key])
+        {
+            [self performSelector:NSSelectorFromString(obj) withObject:selectString];
+        }
+    }];
+
     
+}
+
+- (void)implementObjcMethodWithCurrentSelectString:(NSString *)selectString
+{
+    NSString *methodName = selectString;
+//    if ([methodName matches:@"^[-+].+"]) {
+    methodName = [methodName stringByReplacingOccurrencesOfString:@";" withString:@""];
+    NSLog(@"%@", methodName);
+    [AMIDEHelper openFile:[AMIDEHelper getMFilePathOfCurrentEditFile]];
+    
+    NSTextView *textView = [AMXcodeHelper currentSourceCodeTextView];
+    NSRange textRange = [textView.textStorage.string rangeOfString:methodName options:NSCaseInsensitiveSearch];
+    if (textRange.location == NSNotFound) {
+        NSString *className = [AMIDEHelper getCurrentClassName];
+        NSString *implementationString = [NSString stringWithFormat:@"@implementation %@", className];
+        NSString *endString = @"@end";
+        NSRange startRange = [textView.textStorage.string rangeOfString:implementationString options:NSCaseInsensitiveSearch];
+        NSRange searchRange = NSMakeRange(startRange.location, textView.textStorage.string.length - startRange.location);
+        NSRange endRange = [textView.textStorage.string rangeOfString:endString options:NSCaseInsensitiveSearch range:searchRange];
+        
+        [textView scrollRangeToVisible:endRange];
+        
+        NSString *newString = [NSString stringWithFormat:@"\n%@{\n    \n}\n\n%@", methodName, endString];
+        [textView insertText:newString replacementRange:endRange];
+        
+        [AMIDEHelper selectText:methodName];
+    }
+    [AMIDEHelper selectText:methodName];
+        
+//    }
+}
+
+- (void)implementConstStringWithCurrentSelectString:(NSString *)selectString
+{
+    NSString *methodName = selectString;
+    methodName = [methodName stringByReplacingOccurrencesOfString:@";" withString:@""];
+//    methodName = [methodName stringByReplacingOccurrencesOfString:@"\\n" withString:@""];
+//    NSArray *result = [methodName componentsSeparatedByString:@""];
+    NSRegularExpression *regex = [NSRegularExpression
+                                  regularExpressionWithPattern:@"\\b[\\w\\d]+\\b"
+                                  options:0
+                                  error:NULL];
+
+    NSArray *array   = [regex matchesInString:methodName
+                                               options:0
+                                                 range:NSMakeRange(0, [methodName length])];
+    NSTextCheckingResult *results = array[array.count-1];
+    NSString *result = [methodName substringWithRange:results.range];
+    NSLog(@"result: %@", result);
+    [AMIDEHelper openFile:[AMIDEHelper getMFilePathOfCurrentEditFile]];
+    
+    NSTextView *textView = [AMXcodeHelper currentSourceCodeTextView];
+    NSString *regexString = [NSString stringWithFormat:@"^NSString\\s*\\*\\s*const\\s*%@.+", methodName];
+//    NSRange textRange = [textView.textStorage.string rangeOfString:methodName options:NSCaseInsensitiveSearch];
+    if (![textView.textStorage.string matches:regexString]) {
+        NSString *className = [AMIDEHelper getCurrentClassName];
+        NSString *implementationString = [NSString stringWithFormat:@"@implementation %@", className];
+        NSString *endString = @"@end";
+        NSRange startRange = [textView.textStorage.string rangeOfString:implementationString options:NSCaseInsensitiveSearch];
+        NSRange searchRange = NSMakeRange(startRange.location, textView.textStorage.string.length - startRange.location);
+        NSRange endRange = [textView.textStorage.string rangeOfString:endString options:NSCaseInsensitiveSearch range:searchRange];
+        
+        [textView scrollRangeToVisible:endRange];
+        NSString *stringResult = [NSString stringWithFormat:@"NSString * const %@ = @\"<#value#>\";", result];
+        NSString *newString = [NSString stringWithFormat:@"\n%@\n\n@end", stringResult];
+        [textView insertText:newString replacementRange:endRange];
+        
+        [AMIDEHelper selectText:@"<#value#>"];
+    }
+//    [AMIDEHelper selectText:methodName];
 }
 
 - (void)dealloc
